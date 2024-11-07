195491274eced7f1560af13768fa5bad061c704f
Jessica Dickinson <your_email@example.com>
2024-11-07 02:06:25 -0700

    praticed javascript for the first time, added varibles, alerts, amd combinations

---
+++ index.js
@@ -0,0 +1,20 @@
+
+//make a alert 
+alert("Welcome");
+
+//create a string 
+const greeting = `You have received this message because you have been chosen to open an important vault. Here is the secret combination:`;
+console.log(greeting); // log the message for refrence
+
+//assign three varibles 
+let firstVarible = 5 + 5; //5+5=10
+
+let secondVarible = 8 * 5; //8*5=40
+
+let thirdVarible = 50 - 11; //50-11=39
+
+//create a combination string and show it in an alert box
+const combination = `${firstVarible} - ${secondVarible} - ${thirdVarible}`;
+
+//dialog box displaying the correct code
+alert(greeting + combination);
